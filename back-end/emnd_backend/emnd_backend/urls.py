38d193c3249a9beee73aea151e2385733927ac72
s4322880 <s4322880@student.uq.edu.au>
2017-11-04 13:09:41 +1000

    django environment refresh

--- back-end/emnd_backend/emnd_backend/urls.py
+++ back-end/emnd_backend/emnd_backend/urls.py
@@ -1,4 +1,4 @@
-"""emnd URL Configuration
+"""emnd_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
