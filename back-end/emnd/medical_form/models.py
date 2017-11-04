719b1503b279ecc63b1bfe69e310f84b106f150a
s4322880 <s4322880@student.uq.edu.au>
2017-11-04 12:43:20 +1000

    initial model for questionaire

--- back-end/emnd/medical_form/models.py
+++ back-end/emnd/medical_form/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+#LEXERS = [item for item in get_all_lexers() if item[1]]
+#LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+#STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+
+
+class question(models.Model):
+    submission_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.IntegerField()
+    weight = models.IntegerField()
+    eating_ability = models.IntegerField()
+    depression = models.IntegerField()
+#    class Meta:
+#       ordering = ('created',)
