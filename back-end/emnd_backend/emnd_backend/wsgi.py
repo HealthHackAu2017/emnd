cf54e4ae9fe3a7a57d02c45280a6d704af0eed48
Harsh Pandya <hpand3@gmail.com>
2017-11-04 13:23:24 +1000

    Fixed folder structure

--- back-end/emnd_backend/emnd_backend/wsgi.py
+++ back-end/emnd_backend/emnd_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for emnd project.
+WSGI config for emnd_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emnd.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emnd_backend.settings")
 
 application = get_wsgi_application()
