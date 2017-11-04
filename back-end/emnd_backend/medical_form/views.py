223ef53366155d25da795827cac57e5a27335d39
Harsh Pandya <hpand3@gmail.com>
2017-11-04 13:51:41 +1000

    Swagger is working

--- back-end/emnd_backend/medical_form/views.py
+++ back-end/emnd_backend/medical_form/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Medical Form API')
 
 urlpatterns = [
-    url(r'^$', schema_view)
+    
 ]
