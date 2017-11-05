7707a42f25b06b3fc6cfcd29739ea7dca95e4477
Harsh Pandya <hpand3@gmail.com>
2017-11-05 16:11:04 +1000

    Added analytics stuff

--- back-end/emnd_backend/medical_form/analytics.py
+++ back-end/emnd_backend/medical_form/analytics.py
@@ -12,28 +12,29 @@ def processAnalytics(userId):
   print(submissions[0].__dict__)
   patient_results = pd.DataFrame([submissions[0].__dict__])
 
-  patient_results['d20'] = (patient_results.d20a + patient_results.d20b + 
+  patient_results['Mental Health'] = (patient_results.d20a + patient_results.d20b + 
     (10.0 - patient_results.d20c) + patient_results.d20d)/4.0
 
-  patient_results['d21'] = (patient_results.d21a + (10.0 - patient_results.d21b) + 
+  patient_results['Saliva and Swallowing'] = (patient_results.d21a + (10.0 - patient_results.d21b) + 
     (10.0 - patient_results.d21c) + (10.0 - patient_results.d21d) + 
     patient_results.d21e + (10.0 - patient_results.d21f))/6.0
 
-  patient_results['d22'] = ((10.0 - patient_results.d22a) + (10.0 - patient_results.d22b) +
+  patient_results['Breathing'] = ((10.0 - patient_results.d22a) + (10.0 - patient_results.d22b) +
     (10.0 - patient_results.d22c))/3.0
 
-  patient_results['Overall_Health'] = (patient_results.d20 + patient_results.d21 + 
-    patient_results.d22)/3.0
+  patient_results['Overall Health'] = (patient_results['Mental Health'] + patient_results['Saliva and Swallowing'] + 
+    patient_results['Breathing'])/3.0
 
-  output = patient_results[['d20','d21','d22','Overall_Health']]
+  output = patient_results[['Mental Health','Saliva and Swallowing','Breathing','Overall Health']]
 
   output.to_dict()
-  output['d20']['name'] = 'Mental Health'
-  output['d21']['name'] = 'Saliva and Swallowing'
-  output['d22']['name'] = 'Breathing'
-  output['Overall_Health']['name'] = 'Overall Health'
+  
+  response = {}
+  response['Mental Health'] = output['Mental Health'][0]
+  response['Saliva and Swallowing'] = output['Saliva and Swallowing'][0]
+  response['Breathing'] = output['Breathing'][0]
+  response['Overall Health'] = output['Overall Health'][0]
 
+  print(response)
 
-  print(output)
-
-  return output
+  return response
