9203268a154998036e45d299e274d100d6756a90
Justin Holloway <jholloway@biotelligent.co.uk>
2017-11-05 12:11:48 +1000

    no message

---
+++ emnd-mobile/Emnd/ViewModels/ResultsGraphViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+
+namespace Emnd
+{
+    public class ResultsGraphViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public ResultsGraphViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
+
